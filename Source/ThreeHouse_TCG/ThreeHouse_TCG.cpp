d42f53377da1e6248e9776a4b39199571b07628d
martinGFA <32432545+martinGFA@users.noreply.github.com>
2020-02-04 20:26:10 +0100

    initial commit

---
+++ Source/ThreeHouse_TCG/ThreeHouse_TCG.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "ThreeHouse_TCG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThreeHouse_TCG, "ThreeHouse_TCG" );
