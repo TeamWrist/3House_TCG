bdf99dd870574bd9e8dc50eccbeccafa76074bb4
martinGFA <32432545+martinGFA@users.noreply.github.com>
2020-02-04 21:18:55 +0100

    Revert "d"
    This reverts commit 27e65a55e467f530ad56c153f94ce4aef10a61de.

---
+++ Source/ThreeHouse_TCG/ThreeHouse_TCGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThreeHouse_TCGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THREEHOUSE_TCG_API AThreeHouse_TCGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
